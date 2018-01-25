71c39ff2ce866517d9f71f7262d0c9d972dc6ecb
Evan Norton <evanan39@yahoo.com>
2018-01-25 11:23:21 -0600

    set up App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <h1>text</h1>
+        );
+    }
+}
+
+export default App;
